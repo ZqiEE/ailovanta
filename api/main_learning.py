89e564c635126a7dbfae46906a4c0ceae4ed5d45
ZqiEE <18942619472@163.com>
2026-06-25 13:44:56 +0800

    include guarded learning api

--- api/main_learning.py
+++ api/main_learning.py
@@ -2,7 +2,9 @@ from __future__ import annotations
 
 from api.learning_api import router as learning_router
 from api.learning_foundation_api import router as learning_foundation_router
+from api.learning_gate_api import router as learning_gate_router
 from api.main_packet import app
 
 app.include_router(learning_router)
 app.include_router(learning_foundation_router)
+app.include_router(learning_gate_router)
