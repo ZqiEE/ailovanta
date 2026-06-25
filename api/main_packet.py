b136807d7d08f74525b09bc075e0ede9a70885fb
ZqiEE <18942619472@163.com>
2026-06-25 16:38:30 +0800

    Include parcel export API

--- api/main_packet.py
+++ api/main_packet.py
@@ -2,7 +2,9 @@ from __future__ import annotations
 
 from api.main_owned import app
 from api.outbox_api import router as outbox_router
+from api.parcel_export_api import router as parcel_export_router
 from api.parcel_routes import router
 
 app.include_router(router)
 app.include_router(outbox_router)
+app.include_router(parcel_export_router)
