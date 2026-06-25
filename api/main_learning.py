918e2244d5cbebcca08924778418ad23b9d8a102
ZqiEE <18942619472@163.com>
2026-06-25 14:17:35 +0800

    include artifact binding api

--- api/main_learning.py
+++ api/main_learning.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from api.artifact_binding_api import router as artifact_binding_router
 from api.autonomous_api import router as autonomous_router
 from api.learning_api import router as learning_router
 from api.learning_foundation_api import router as learning_foundation_router
@@ -14,3 +15,4 @@ app.include_router(learning_gate_router)
 app.include_router(model_monitor_router)
 app.include_router(rollback_router)
 app.include_router(autonomous_router)
+app.include_router(artifact_binding_router)
