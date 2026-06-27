ff71b3c009985e80cf0031593ea93fc57ad0c5b4
ZqiEE <18942619472@163.com>
2026-06-27 22:29:32 +0800

    wire routes

--- api/main_code.py
+++ api/main_code.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from api.catalog_routes import router as catalog_router
+from api.catalog_extra_routes import router as extra_router
 from api.code_chat_api import router as code_chat_router
 from api.code_result_api import router as code_result_router
 from api.main import app
@@ -8,3 +9,4 @@ from api.main import app
 app.include_router(code_chat_router)
 app.include_router(code_result_router)
 app.include_router(catalog_router)
+app.include_router(extra_router)
