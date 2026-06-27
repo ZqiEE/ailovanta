404d664e84088f8f6025cec172a41e57468094c7
ZqiEE <18942619472@163.com>
2026-06-28 02:16:47 +0800

    mount rollback api in incident entry

--- api/main_incident_ready.py
+++ api/main_incident_ready.py
@@ -2,5 +2,7 @@ from __future__ import annotations
 
 from api.incident_routes import router as incident_router
 from api.main_monitor_ready import app
+from api.rollback_api import router as rollback_router
 
 app.include_router(incident_router)
+app.include_router(rollback_router)
