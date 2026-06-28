a31ef75bb157253ed6b20c92f97be7c9629d37cd
ZqiEE <18942619472@163.com>
2026-06-28 16:24:57 +0800

    mount result guard readiness route

--- api/main_release_ready.py
+++ api/main_release_ready.py
@@ -3,6 +3,8 @@ from __future__ import annotations
 from api.default_chat_probe_routes import router as default_chat_probe_router
 from api.final_gate_routes import router as final_gate_router
 from api.main_incident_ready import app
+from api.result_guard_routes import router as result_guard_router
 
 app.include_router(final_gate_router)
 app.include_router(default_chat_probe_router)
+app.include_router(result_guard_router)
