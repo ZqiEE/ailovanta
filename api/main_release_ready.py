1cbee25dfa1e16ecaf13b2a58cd6839ab2d3133d
ZqiEE <18942619472@163.com>
2026-07-02 20:31:13 +0800

    mount autotrain routes in release app

--- api/main_release_ready.py
+++ api/main_release_ready.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from api.artifact_binding_api import router as artifact_binding_router
+from api.autotrain_api import router as autotrain_router
 from api.core_result_api import router as core_result_router
 from api.default_chat_probe_routes import router as default_chat_probe_router
 from api.final_gate_routes import router as final_gate_router
@@ -8,6 +9,7 @@ from api.foundation_artifact_routes import router as far
 from api.foundation_job_api import router as foundation_job_router
 from api.foundation_pipeline_api import router as foundation_pipeline_router
 from api.foundation_result_api import router as foundation_result_router
+from api.learning_api import router as learning_router
 from api.learning_foundation_api import router as learning_foundation_router
 from api.learning_gate_api import router as learning_gate_router
 from api.main_incident_ready import app
@@ -22,7 +24,9 @@ app.include_router(core_result_router)
 app.include_router(foundation_job_router)
 app.include_router(foundation_result_router)
 app.include_router(foundation_pipeline_router)
+app.include_router(learning_router)
 app.include_router(learning_foundation_router)
 app.include_router(learning_gate_router)
+app.include_router(autotrain_router)
 app.include_router(artifact_binding_router)
 app.include_router(model_monitor_router)
