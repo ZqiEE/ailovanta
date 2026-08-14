c1539347a4e9e54ac92039f4b043dab5e8dae6de
ZqiEE <18942619472@163.com>
2026-08-14 17:44:47 +0800

    add one-command windows local startup

---
+++ start-local.ps1
@@ -0,0 +1,18 @@
+$ErrorActionPreference = "Stop"
+Set-Location $PSScriptRoot
+
+$Python = $null
+if (Get-Command py -ErrorAction SilentlyContinue) {
+    $Python = "py"
+} elseif (Get-Command python -ErrorAction SilentlyContinue) {
+    $Python = "python"
+} else {
+    Write-Error "Python 3.11+ is required. Install Python, then rerun .\start-local.ps1"
+}
+
+if (-not (Test-Path ".venv")) {
+    if ($Python -eq "py") { & py -3 -m venv .venv } else { & python -m venv .venv }
+}
+
+& .\.venv\Scripts\python.exe -m pip install --disable-pip-version-check -q -r requirements-coding.txt
+& .\.venv\Scripts\python.exe -m node_client.local_runtime
